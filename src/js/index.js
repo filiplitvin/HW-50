e4918d766cc08f2880b5c4727ff76691c03bdde8
Filip Litvin <litvinfilip5@gmail.com>
2025-12-27 18:09:15 +0200

    виконав всі завдання

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+//task 1
+const celsius = -11;
+const fahrenheit = -11 * 1.8 + 32;
+console.log(fahrenheit);
+//task 2
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = 24 * 60 * daysInMonth;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+// task 3
+let health = 100 - 50;
+let energy = 100 - 50;
+console.log(health);
+console.log(energy);
+// task 4
+const totalPrice = 100;
+const discountedPrice = (10 * 100) / 100;
+console.log(discountedPrice);
+// ???
+// task 5
+const floatNumber = 12.78;
+console.log(Math.floor(floatNumber));
+const roundedDown = 12;
+// task 6
+const floatString = "45.67";
+console.log(parseFloat(floatString));
+parseFloat(45.67);
+// task 7
+const intString = "123";
+const intStringIntoNumber = Number(intString);
+console.log(intString);
+// task 8
+const number = 16;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+// task 9
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = Number(stringNumber);
+console.log(stringNumber);
+const convertedString = toString(42);
+console.log(convertedString);
