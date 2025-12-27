1aad9626ddb382e74b33012d0d5ae421dfa30334
Filip Litvin <litvinfilip5@gmail.com>
2025-12-27 21:04:59 +0200

    виправив помилки в завданнях

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,39 @@
 //task 1
 const celsius = -11;
-const fahrenheit = -11 * 1.8 + 32;
+const fahrenheit = celsius * 1.8 + 32;
 console.log(fahrenheit);
 //task 2
 const daysInMonth = 30;
-const hoursInMonth = daysInMonth * 24;
-const minutesInMonth = 24 * 60 * daysInMonth;
-console.log(hoursInMonth);
-console.log(minutesInMonth);
+const minutesInMonth = 60;
+const hoursInMonth = 24;
+const allHoursInMonth = hoursInMonth * daysInMonth;
+const allMinInMonth = hoursInMonth * minutesInMonth * daysInMonth;
+console.log(allHoursInMonth);
+console.log(allMinInMonth);
 // task 3
-let health = 100 - 50;
-let energy = 100 - 50;
+let health = 100;
+let energy = 100;
+health = health - 50;
+energy = energy - 50;
 console.log(health);
 console.log(energy);
 // task 4
 const totalPrice = 100;
-const discountedPrice = (10 * 100) / 100;
+const discountedPrice = (totalPrice / 100) * 10;
 console.log(discountedPrice);
 // ???
 // task 5
 const floatNumber = 12.78;
-console.log(Math.floor(floatNumber));
-const roundedDown = 12;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
 // task 6
 const floatString = "45.67";
-console.log(parseFloat(floatString));
-parseFloat(45.67);
+const parsedFloat = Math.floor(floatString);
+console.log(parsedFloat);
 // task 7
 const intString = "123";
-const intStringIntoNumber = Number(intString);
-console.log(intString);
+const intStringIntoNumber = parseInt(intString);
+console.log(intStringIntoNumber);
 // task 8
 const number = 16;
 const sqrtNumber = Math.sqrt(number);
@@ -39,5 +43,5 @@ const integer = 42;
 const stringNumber = "256";
 const convertedInt = Number(stringNumber);
 console.log(stringNumber);
-const convertedString = toString(42);
+const convertedString = integer.toString();
 console.log(convertedString);
